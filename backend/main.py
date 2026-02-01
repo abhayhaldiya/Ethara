796a0814d7b93d066ff1d1ede258a86d07cc2104
abhayhaldiya <abhayhaldiya@gmail.com>
2026-02-01 23:21:08 +0530

    allowed all cors for now

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="HRMS Lite API", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("CORS_ORIGINS", "http://localhost:3000").split(","),
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
