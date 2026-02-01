3dc284dce88a1d7e102487b358dc9d0f0e8b421e
abhayhaldiya <abhayhaldiya@gmail.com>
2026-02-01 23:22:07 +0530

    fixed error

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="HRMS Lite API", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
